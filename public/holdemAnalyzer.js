a039b43c9c8dd9ad0b3b80a01ddf5cb38cf68e5c
Matt DuLeone <matt@duleone.com>
2016-02-07 15:21:36 -0500

    fixing random int generator so that max is not probability 0

--- public/holdemAnalyzer.js
+++ public/holdemAnalyzer.js
@@ -4,7 +4,7 @@ function randInt(min, max) {
         var max = min--;
         min = 0;
     }
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.min(Math.floor(Math.random() * (max - min + 1)) + min, max);
 }
 
 var shuffle = function (array) {
